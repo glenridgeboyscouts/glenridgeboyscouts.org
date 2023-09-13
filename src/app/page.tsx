b4ff059fc73ec12bc12f1792cf4e3d1293de3048
rolandwunderlich <roland@wcopy.net>
2023-09-12 20:21:13 -0400

    Fix home page popcorn button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
 
 								<div className='mt-4 md:mt-8'>
 									<a
-										href='/popcorn/'
+										href='/popcorn'
 										className='inline-block rounded bg-emerald-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-emerald-700 focus:outline-none focus:ring focus:ring-yellow-400'
 									>
 										Order now!
